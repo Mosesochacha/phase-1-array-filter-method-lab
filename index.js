f70b2153005e48b461ccfe1a6357c44255689079
mosesochacha <ochachamoses443@gmail.com>
2022-11-29 18:49:53 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+ const drivers=["Bobby","Sammy","Sally" ,"Annette","Sarah","Bobby"]
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver){ 
+        return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+  findMatching();
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase().substring(
+            0, attribute.length) === attribute.toLowerCase() })
+  }
+  fuzzyMatch();
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver){ 
+        return driver.name === argument })
+  }
+  matchName;
